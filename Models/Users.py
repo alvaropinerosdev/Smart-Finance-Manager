4ff8db5324f17f0962b3153846b63c88e7d4720f
alvaro0777 <alvaro.pineros.dev@gmail.com>
2026-07-15 18:10:29 -0500

    feat: create User class with user attributes

---
+++ Models/Users.py
@@ -0,0 +1,13 @@
+class User:
+    def __init__(self,userId, username, firstName, lastName, email, password_hash, createdAt):
+        self.userId = userId
+        self.username = username
+        self.firstName = firstName
+        self.lastName = lastName
+        self.email = email
+        self.password_hash = password_hash
+        self.createdAt = createdAt
+
+    
+
+    
